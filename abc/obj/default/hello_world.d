1b9db6fa2fa7c39016116eb8436d645df553a2d4
Bapu Pruthvidhar <talktobpd@icloud.com>
2022-04-17 22:16:22 +0530

    Add files via upload

---
+++ abc/obj/default/hello_world.d
@@ -0,0 +1,10 @@
+obj/default/hello_world.o: hello_world.c ../abc_bsp//HAL/inc/io.h \
+ ../abc_bsp//HAL/inc/alt_types.h ../abc_bsp/system.h ../abc_bsp/linker.h
+
+../abc_bsp//HAL/inc/io.h:
+
+../abc_bsp//HAL/inc/alt_types.h:
+
+../abc_bsp/system.h:
+
+../abc_bsp/linker.h:
